400d435e2b39fbf8741bbf24ffe570f9784e63ee
M Mandel <mbmandel7@gmail.com>
2014-10-07 21:06:22 -0400

    Format Corrections

--- src/stein/chatroom/ClientListeningThread.java
+++ src/stein/chatroom/ClientListeningThread.java
@@ -11,45 +11,48 @@ import java.net.Socket;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class ClientListeningThread extends Thread{
-	
-/*In order for the client to receive messages from the server, it must "listen out" for them.
- * I am doing this in a separate thread, so that it can basically "run in the background,"
- * allowing the chat to be bidirectional. The client will be listening out for the server,
- * and whenever the server sends it a message it will "print it out" -- but in the meantime
- * the client can continue chatting without waiting for a response from the server.
- */
-	
-private Socket socket;
-private ChatScreen screen;
+public class ClientListeningThread extends Thread {
 
-public ClientListeningThread(Socket socket, ChatScreen screen){
-	this.screen = screen;
-	this.socket = socket;
-}
+	/*
+	 * In order for the client to receive messages from the server, it must
+	 * "listen out" for them. I am doing this in a separate thread, so that it
+	 * can basically "run in the background," allowing the chat to be
+	 * bidirectional. The client will be listening out for the server, and
+	 * whenever the server sends it a message it will "print it out" -- but in
+	 * the meantime the client can continue chatting without waiting for a
+	 * response from the server.
+	 */
 
-public void run(){
-	//Get the input stream
-	InputStream input=null;
-	try {
-		input = this.socket.getInputStream();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	private Socket socket;
+	private ChatScreen screen;
+
+	public ClientListeningThread(Socket socket, ChatScreen screen) {
+		this.screen = screen;
+		this.socket = socket;
 	}
-	BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-	//In an infinite loop -- it should constantly listen!
-	String text;
-	try {
-		while((text = reader.readLine())!=null){
-			if(!"".equals(text)){
-				UpdateGUIThread update = new UpdateGUIThread(this.screen, text, Color.GREEN);
-				update.start();
-			}
+
+	public void run() {
+		// Get the input stream
+		InputStream input = null;
+		try {
+			input = this.socket.getInputStream();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+		// In an infinite loop -- it should constantly listen!
+		String text;
+		try {
+			while ((text = reader.readLine()) != null) {
+				if (!"".equals(text)) {
+					UpdateGUIThread update = new UpdateGUIThread(this.screen, text, Color.GREEN);
+					update.start();
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-}
 }
--- src/stein/chatroom/UpdateGUIThread.java
+++ src/stein/chatroom/UpdateGUIThread.java
@@ -7,31 +7,30 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-public class UpdateGUIThread extends Thread{
-	
+public class UpdateGUIThread extends Thread {
+
 	private JLabel convoPiece;
 	private ChatScreen screen;
-	//private ConversationComponent convoComponent;
+	// private ConversationComponent convoComponent;
 	private String text;
 	private Color color;
-	
-	
-	public UpdateGUIThread(ChatScreen screen, String text, Color color){
+
+	public UpdateGUIThread(ChatScreen screen, String text, Color color) {
 		this.screen = screen;
-		//this.convoComponent = convoComp;
+		// this.convoComponent = convoComp;
 		this.convoPiece = screen.getCurrentConvoLabel();
 		this.text = text;
 		this.color = color;
 	}
-	
-	public void run(){
+
+	public void run() {
 		this.screen.incrementCounter();
 		this.convoPiece.setOpaque(true);
 		this.convoPiece.setText(this.text);
 		this.convoPiece.setBackground(this.color);
 		this.convoPiece.repaint();
 		this.screen.repaint();
-		
+
 	}
 
 }
